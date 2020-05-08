f2d133b1c7bfa0f9f47b6cdddd7d6bba760714f4
Crowdbotics <team@crowdbotics.com>
2020-05-08 06:01:20 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/guess_that_tv_show_16748/MainActivity.java
@@ -0,0 +1,15 @@
+package com.guess_that_tv_show_16748;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "guess_that_tv_show_16748";
+    }
+}
